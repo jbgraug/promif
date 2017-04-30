a856152475cbd88dacd06eadad6df508d4517c1e
Juan Grau Gimeno <juan.grau.gimeno@wolterskluwer.com>
2017-04-30 16:16:52 +0200

    initial implementation

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ *  Promif: Run promise base work flows.
+ *  Refer to README.MD and CHANGELOG.MD
+ *
+ *  @author {Juan Grau <jbgraug@gmail.com>}
+ */
+
+const promif = require('./lib/promif');
+
+/**
+ *
+ */
+module.exports = promif;
